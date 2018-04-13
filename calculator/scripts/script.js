be1ecd6792c299cd2939c55fc4ccba1d7328d5f4
Reka Kulinyi <reekaa85@gmail.com>
2018-04-13 19:53:36 +0200

    basic functions added

--- calculator/scripts/script.js
+++ calculator/scripts/script.js
@@ -1,11 +1,39 @@
 document.addEventListener("DOMContentLoaded",function(){
-  const calcElements = document.querySelectorAll("td");
   const screen = document.getElementById("screen-value");
+  screen.value = "";
+  const calcNumbers = document.querySelectorAll(".num");
+  const calcOperators = document.querySelectorAll(".operator")
+  const clear = document.getElementById("clear");
+  const equal = document.getElementById("equal");
+  let operations = new String();
 
-  calcElements.forEach(function(button){
+  calcNumbers.forEach(function(button){
     button.addEventListener("click", function(){
-      screen.value = (button.innerText);
+      if (screen.value === "" || parseInt(screen.value, 10)){
+        screen.value += button.innerText;
+      }else{
+        screen.value = "";
+        screen.value += button.innerText;
+      }
+      operations += button.innerText;
     });
+  });
+
+  calcOperators.forEach(function(button){
+    button.addEventListener("click", function(){
+      screen.value = button.innerText;
+      operations += button.innerText;
+    });
+  });
+
+  clear.addEventListener('click', function(){
+    screen.value = "";
+    operations = "";
+  });
+
+  equal.addEventListener("click", function(){
+    screen.value = (eval(operations));
+    operations = "";
   });
 });
 
