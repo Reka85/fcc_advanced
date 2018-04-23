690e5d839ebb9e17b859942be9f527391a9e4b1e
Reka Kulinyi <reekaa85@gmail.com>
2018-04-23 15:38:42 +0200

    calculator fixed

--- calculator/scripts/script.js
+++ calculator/scripts/script.js
@@ -1,60 +1,62 @@
 document.addEventListener("DOMContentLoaded",function(){
   const screen = document.getElementById("screen-value");
-  screen.value = "";
-  const calcNumbers = document.querySelectorAll(".num");
-  const calcOperators = document.querySelectorAll(".operator");
+  screen.textContent = "";
+  const calcNumbers = document.querySelectorAll(".num");//number buttons from 0 to 9
+  const calcOperators = document.querySelectorAll(".operator");// +,-,/,* buttons
   const clear = document.getElementById("clear");
   const bspace = document.getElementById("bspace");
   const equal = document.getElementById("equal");
+  const point = document.getElementById("point");
   const operators = {"incr": "+", "decr": "-", "division": "/", "multiplication": "*"};
   const numbers = [0,1,2,3,4,5,6,7,8,9];
-  const point = document.getElementById("point");
   let operations = new String();
 
   calcNumbers.forEach(function(button){
     button.addEventListener("click", function(){
-      if (equal.classList.contains("active")){//if the last button pressed is equal
-        screen.innerText = "";
+      //if the last button that was pressed is the equal button
+      if (equal.classList.contains("active")){
+        screen.textContent = "";
         equal.classList.remove("active");
-      }// fcc utolso feladat
-      else if (screen.innerText === "" || screen.innerText === parseInt(screen.innerText, 10).toString() || screen.innerText.slice(-1) === "."){
+      }
+      // if screen is empty or there is already a number or the last character is a decimal point
+      else if (screen.textContent === "" || screen.textContent === parseInt(screen.textContent, 10).toString() || screen.textContent.slice(-1) === "."){
         //pass
       }else{
-        screen.innerText = "";
+        screen.textContent = "";
       }
-      screen.innerText += numbers[button.innerText].toString();
-      operations += numbers[button.innerText].toString();
+      screen.textContent += numbers[button.textContent].toString();
+      operations += numbers[button.textContent].toString();
     });
   });
 
   calcOperators.forEach(function(button){
     button.addEventListener("click", function(){
-      screen.innerText = operators[button.getAttribute('data-op')];
+      screen.textContent = operators[button.getAttribute('data-op')];
       operations += operators[button.getAttribute('data-op')];
     });
   });
 
   clear.addEventListener('click', function(){
-    screen.innerText = "";
+    screen.textContent = "";
     operations = "";
   });
 
   equal.addEventListener("click", function(){
-    screen.innerText = (eval(operations)); // see eval
+    screen.textContent = (eval(operations)); // see eval
     operations = "";
     equal.classList.add("active");
   });
 
   bspace.addEventListener("click", function(){
-    if (screen.innerText !== ""){
-      screen.innerText = screen.innerText.slice(0, -1);
+    if (screen.textContent !== ""){
+      screen.textContent = screen.textContent.slice(0, -1);
       operations = operations.slice(0, -1);
     }
   });
 
   point.addEventListener("click", function(){
-    if (screen.innerText === parseInt(screen.innerText, 10).toString()){
-      screen.innerText += ".";
+    if (screen.textContent === parseInt(screen.textContent, 10).toString()){
+      screen.textContent += ".";
       operations += ".";
     }
   });
