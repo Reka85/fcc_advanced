9f091cf50fda42999e3e4f940449d39d62d24474
Reka Kulinyi <reekaa85@gmail.com>
2018-04-26 15:43:43 +0200

    updated equal

--- calculator/scripts/script.js
+++ calculator/scripts/script.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded",function(){
   const screen = document.getElementById("screen-value");
   screen.textContent = "";
-  const calcNumbers = document.querySelectorAll(".num");//number buttons from 0 to 9
-  const calcOperators = document.querySelectorAll(".operator");// +,-,/,* buttons
+  const calcNumbers = Array.from(document.getElementsByClassName("num"));//number buttons from 0 to 9
+  const calcOperators = Array.from(document.getElementsByClassName("operator"));// +,-,/,* buttons
   const clear = document.getElementById("clear");
   const bspace = document.getElementById("bspace");
   const equal = document.getElementById("equal");
@@ -11,12 +11,15 @@ document.addEventListener("DOMContentLoaded",function(){
   const numbers = [0,1,2,3,4,5,6,7,8,9];
   let operations = new String();
 
+  let equalClicked = false;
+
   calcNumbers.forEach(function(button){
     button.addEventListener("click", function(){
       //if the last button that was pressed is the equal button
-      if (equal.classList.contains("active")){
+      if (equalClicked){
+        operations = "";
         screen.textContent = "";
-        equal.classList.remove("active");
+        equalClicked = false;
       }
       // if screen is empty or there is already a number or the last character is a decimal point
       else if (screen.textContent === "" || screen.textContent === parseInt(screen.textContent, 10).toString() || screen.textContent.slice(-1) === "."){
@@ -36,15 +39,19 @@ document.addEventListener("DOMContentLoaded",function(){
     });
   });
 
-  clear.addEventListener('click', function(){
+  clear.addEventListener("click", function(){
     screen.textContent = "";
     operations = "";
   });
 
   equal.addEventListener("click", function(){
-    screen.textContent = (eval(operations)); // see eval
-    operations = "";
-    equal.classList.add("active");
+    // if enter is clicked multiple times we repeat the last operation
+    if (equalClicked){
+      const reg = /(\-|\+|\/|\*)[0-9]+$/;
+      operations += operations.match(reg)[0];
+    }
+    screen.textContent = (eval(operations));
+    equalClicked = true;
   });
 
   bspace.addEventListener("click", function(){
