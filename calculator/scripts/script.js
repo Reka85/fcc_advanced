58619505add0142522cda17cf6c57d22d2d2330f
Reka Kulinyi <reekaa85@gmail.com>
2018-04-26 19:16:06 +0200

    refactoring

--- calculator/scripts/script.js
+++ calculator/scripts/script.js
@@ -8,65 +8,67 @@ document.addEventListener("DOMContentLoaded",function(){
   const equal = document.getElementById("equal");
   const point = document.getElementById("point");
   const operators = {"incr": "+", "decr": "-", "division": "/", "multiplication": "*"};
-  const numbers = [0,1,2,3,4,5,6,7,8,9];
+  const numbers = ['0','1','2','3','4','5','6','7','8','9'];
   let operations = new String();
-
+//equal vegeredményét tovább modositani
+//function def külön az eventlistenertöl
+//screen.textContent helyett vmi
   let equalClicked = false;
 
   calcNumbers.forEach(function(button){
     button.addEventListener("click", function(){
       //if the last button that was pressed is the equal button
       if (equalClicked){
-        operations = "";
-        screen.textContent = "";
+        clearOperationsAndScreen();
         equalClicked = false;
-      }
-      // if screen is empty or there is already a number or the last character is a decimal point
-      else if (screen.textContent === "" || screen.textContent === parseInt(screen.textContent, 10).toString() || screen.textContent.slice(-1) === "."){
-        //pass
-      }else{
+      // if last element is an operator clear screen
+      } else if (Object.values(operators).includes(operations.slice(-1))) {
         screen.textContent = "";
       }
-      screen.textContent += numbers[button.textContent].toString();
-      operations += numbers[button.textContent].toString();
+      addSameTextToOperationsAndScreen(numbers[this.textContent]);
     });
   });
 
   calcOperators.forEach(function(button){
-    button.addEventListener("click", function(){
-      screen.textContent = operators[button.getAttribute('data-op')];
-      operations += operators[button.getAttribute('data-op')];
+    button.addEventListener("click", function(e){
+      screen.textContent = operators[e.target.getAttribute('data-op')];
+      operations += operators[e.target.getAttribute('data-op')];
     });
   });
 
-  clear.addEventListener("click", function(){
-    screen.textContent = "";
-    operations = "";
-  });
+  clear.addEventListener("click", clearOperationsAndScreen);
 
   equal.addEventListener("click", function(){
-    // if enter is clicked multiple times we repeat the last operation
+    // if equal is clicked multiple times we repeat the last operation
     if (equalClicked){
       const reg = /(\-|\+|\/|\*)[0-9]+$/;
       operations += operations.match(reg)[0];
     }
-    screen.textContent = (eval(operations));
+    screen.textContent = (eval(operations));// get result
     equalClicked = true;
   });
 
   bspace.addEventListener("click", function(){
-    if (screen.textContent !== ""){
-      screen.textContent = screen.textContent.slice(0, -1);
-      operations = operations.slice(0, -1);
-    }
+    screen.textContent = screen.textContent.slice(0, -1);
+    operations = operations.slice(0, -1);
   });
 
   point.addEventListener("click", function(){
-    if (screen.textContent === parseInt(screen.textContent, 10).toString()){
-      screen.textContent += ".";
-      operations += ".";
+    // if last element is a number we add decimal point
+    if (operations.slice(-1) === parseInt(operations.slice(-1), 10).toString()){
+      addSameTextToOperationsAndScreen(".");
     }
   });
+
+  function clearOperationsAndScreen(){
+    operations = "";
+    screen.textContent = "";
+  }
+
+  function addSameTextToOperationsAndScreen(str){
+    screen.textContent += str;
+    operations += str;
+  }
 });
 
 
