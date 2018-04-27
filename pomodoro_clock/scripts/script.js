e952e9fb8ff0b6aa65c61057f7aa5b6baf3cabad
Reka Kulinyi <reekaa85@gmail.com>
2018-04-27 14:40:05 +0200

    minutes countdown setup

--- pomodoro_clock/scripts/script.js
+++ pomodoro_clock/scripts/script.js
@@ -1,55 +1,87 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-  const pause = document.getElementById("pause");
-  const time = document.getElementById("time");
+  const pauseTime = document.getElementById("pause");
+  const workTime = document.getElementById("time");
   const minutesLeft = document.getElementById("minutes-left");
   const secondsLeft = document.getElementById("seconds-left");
+  const startButton = document.getElementById("start-button");
 
-  let set_time_minutes = 25;
-  let set_pause = 5;
-  pause.textContent = set_pause.toString();
-  time.textContent = set_time_minutes.toString();
-  let set_time_seconds = 0;
-  minutesLeft.textContent = set_time_minutes;
+  let setMinutes = 4;
+  let setPause = 5;
+  let setSeconds = 0;
+  pauseTime.textContent = setPause.toString();
+  workTime.textContent = setMinutes.toString();
+  minutesLeft.textContent = setMinutes.toString();
 
 
-  if (set_time_seconds === 0){
+  if (setSeconds === 0){
     secondsLeft.textContent = "00";
   }
 
+  if (setMinutes <= 9){
+    minutesLeft.textContent = `0${setMinutes}`;
+  }
 
   document.getElementById("pause-increase").addEventListener("click", function(){
-    set_pause += 1;
-    pause.textContent = set_pause.toString();
+    setPause += 1;
+    pauseTime.textContent = setPause.toString();
   });
 
   document.getElementById("pause-decrease").addEventListener("click", function(){
-    if (set_pause > 0){
-      set_pause -= 1;
-      pause.textContent = set_pause.toString();
+    if (setPause > 0){
+      setPause -= 1;
+      pauseTime.textContent = setPause.toString();
     }
   });
 
   document.getElementById("time-increase").addEventListener("click", function(){
-    set_time_minutes += 1;
-    time.textContent = set_time_minutes.toString();
-    minutesLeft.textContent = set_time_minutes.toString();
+    setMinutes += 1;
+    if(setMinutes <= 9){
+      minutesLeft.textContent = `0${setMinutes.toString()}`;
+    }else{
+      workTime.textContent = setMinutes.toString();
+      minutesLeft.textContent = setMinutes.toString();
+    }
   });
 
   document.getElementById("time-decrease").addEventListener("click", function(){
-    set_time_minutes -= 1;
-    time.textContent = set_time_minutes.toString();
-    minutesLeft.textContent = set_time_minutes.toString();
+    if (setMinutes > 0){
+      setMinutes -= 1;
+      if(setMinutes <= 9){
+        minutesLeft.textContent = `0${setMinutes.toString()}`;
+      }else{
+        workTime.textContent = setMinutes.toString();
+        minutesLeft.textContent = setMinutes.toString();
+      }
+    }
   });
 
-  // minutes.addEventListener("click", function(){
-  //   while (set_minutes > 0){
-  //     setTimeout(function(){
-  //       set_minutes -= 1;
-  //       minutes.innerText = set_minutes;
-  //     },1000);
-  //   }
-  // });
+  startButton.addEventListener("click", function(){
+      //setMinutes--;
+      if (setMinutes <= 9){
+        minutesLeft.textContent = `0${setMinutes}`;
+      }
+      const minTimer = setInterval(function(){
+        setMinutes--;
+        if (setMinutes <= 9){
+          minutesLeft.textContent = `0${setMinutes}`;
+        }
+        if (setMinutes === 0){
+          clearInterval(minTimer);
+        }
+      },60000);
 
+      // let seconds = parseInt(secondsLeft.textContent, 10);
+      // const secTimer = setInterval(function(){
+      //   if (secondsLeft.textContent === "00" || secondsLeft.textContent === "0"){
+      //     seconds = 60;
+      //   }
+      //   seconds--;
+      //   secondsLeft.textContent = seconds.toString();
+      //   if (setMinutes === 0 && secondsLeft.textContent === "00" || secondsLeft.textContent === "0"){
+      //     clearInterval(secTimer);
+      //   }
+      // },1000)
+  });
 });
 
