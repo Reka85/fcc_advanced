878d3b6cb31012d1fb071d9c0068dc0856882acc
Reka Kulinyi <reekaa85@gmail.com>
2018-05-04 22:29:28 +0200

    refactoring, audio added

--- pomodoro_clock/scripts/script.js
+++ pomodoro_clock/scripts/script.js
@@ -1,19 +1,19 @@
 document.addEventListener("DOMContentLoaded", function(){
-  //const sound = document.getElementById("sound")[0];
-  //sound.play();
+  const sound = document.getElementById("sound");
+
   const pauseTime = document.getElementById("pause");
   const sessionTime = document.getElementById("time");
-  const minutesLeft = document.getElementById("minutes-left");
-  const secondsLeft = document.getElementById("seconds-left");
   const startButton = document.getElementById("start-button");
   const resetButton = document.getElementById("reset-button");
   const buttons = Array.from(document.getElementsByClassName("buttons"))
+  const clock = document.getElementById("clock");
+  const timeType = document.getElementById("time-type");
+
   let setSession = parseInt(sessionTime.textContent, 10);
   let setPause = parseInt(pauseTime.textContent, 10);
   let seconds = 0;
 
-  addZeroIfOneDigit(setSession, minutesLeft);
-  addZeroIfOneDigit(seconds, secondsLeft);
+  refreshClock(setSession, seconds);
 
   document.getElementById("pause-increase").addEventListener("click", function(){
     setPause += 1;
@@ -29,101 +29,102 @@ document.addEventListener("DOMContentLoaded", function(){
 
   document.getElementById("time-increase").addEventListener("click", function(){
     setSession += 1;
-    addZeroIfOneDigit(setSession, minutesLeft);
+    refreshClock(setSession, seconds);
     sessionTime.textContent = setSession;
   });
 
   document.getElementById("time-decrease").addEventListener("click", function(){
     if (setSession > 1){
       setSession -= 1;
-      addZeroIfOneDigit(setSession, minutesLeft);
+      refreshClock(setSession, seconds);
       sessionTime.textContent = setSession;
     }
   });
 
-  let reseted=false
+  let reseted = false
 
   startButton.addEventListener("click", function(){
-    addZeroIfOneDigit(setSession, minutesLeft);
-    addZeroIfOneDigit(seconds, secondsLeft);
+    refreshClock(setSession, seconds);
+
     reseted = false;
-    countDown(setSession,setPause, minutesLeft, secondsLeft);
+    countDown(setSession,setPause);
   });
 
   resetButton.addEventListener("click", function(){
     reseted=true;
-    addZeroIfOneDigit(setSession, minutesLeft);
-    addZeroIfOneDigit(seconds, secondsLeft);
+    refreshClock(setSession, seconds);
+
     buttons.map((elem) => (elem.style.display = "inline"));
-    document.getElementById("time-type").innerHTML = "";
+    timeType.innerHTML = "";
     this.style.display = "none";
   });
 
 
-  function countDown(mins, pause, minField, secField){
+  function countDown(mins, pause){
     var secs = mins * 60;
     let pauseSecs = pause * 60
     mins--;
     pause--;
     buttons.map((elem) => (elem.style.display = "none"));
     resetButton.style.display = "inline";
-    const counter = setInterval(timer, 400);
+    const counter = setInterval(timer, 1000);
 
-    document.getElementById("time-type").innerHTML = "<p>Session time left:</p>"
+    timeType.innerHTML = "<p>Session time left:</p>"
 
+    // session countdown starts
     function timer(){
-      console.log(mins, secs);
       if (reseted){
         clearInterval(counter);
       }
       else if (mins === 0 && secs === 0){
-        //ping!
+        sound.play();
         clearInterval(counter);
-        var breakStart = setInterval(breakTimer, 400);
-        addZeroIfOneDigit(mins, minField);
-        addZeroIfOneDigit(secs % 60, secField);
+        var breakStart = setInterval(breakTimer, 1000);
+        refreshClock(mins, secs);
       }
       else if (mins >= 0){
         secs--;
-        addZeroIfOneDigit(mins, minField);
-        addZeroIfOneDigit(secs % 60, secField);
+        refreshClock(mins, secs);
         if (secs % 60 === 0 && mins !== 0){
           mins--;
         }
       }
 
+      // break countdown starts
       function breakTimer(){
-        document.getElementById("time-type").innerHTML = "<p>Break time left:</p>"
+        timeType.innerHTML = "<p>Break time left:</p>"
         if (reseted){
           clearInterval(breakStart);
         }
         else if (pause === 0 && pauseSecs === 0){
-          //ping!
+          sound.play();
           clearInterval(breakStart);
           buttons.map((elem) => (elem.style.display = "inline"));
           resetButton.style.display = "none";
-          document.getElementById("time-type").innerHTML = "<p>You finished a cycle</p>"
-          addZeroIfOneDigit(mins, minField);
-          addZeroIfOneDigit(secs % 60, secField);
+          timeType.innerHTML = "<p>You finished a cycle</p>"
+          refreshClock(mins, secs);
         }
         else if (pause >= 0){
           pauseSecs--;
           if (pauseSecs % 60 === 0 && pause !== 0){
             pause--;
           }
-        addZeroIfOneDigit(pause, minField);
-        addZeroIfOneDigit(pauseSecs % 60, secField);
+        refreshClock(pause, pauseSecs);
         }
       }
     };
   }
 
-  function addZeroIfOneDigit(timeUnit, elem){
-    if (timeUnit <= 9){
-      elem.textContent = `0${timeUnit}`;
-    } else {
-      elem.textContent = timeUnit;
+  function refreshClock(minutes, seconds){
+    seconds = seconds % 60;
+    if (minutes < 10){
+      minutes = `0${minutes}`
     }
+
+    if (seconds < 10){
+      seconds = `0${seconds}`
+    }
+    clock.innerHTML = `<span>${minutes}</span>:<span>${seconds}</span>`;
   }
 });
 
