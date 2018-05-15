0ea66881b79a91ca1757343b52cd2ed6742005b7
Reka Kulinyi <reekaa85@gmail.com>
2018-05-15 16:48:08 +0200

    reset and strict mode added

--- simon_game/scripts/script.js
+++ simon_game/scripts/script.js
@@ -1,23 +1,43 @@
 document.addEventListener("DOMContentLoaded", function(){
   const arr = [0,1,2,3];
-
   let chosenNumbers = [];
   let userNumbers = [];
+  const circles = Array.from(document.getElementsByClassName("circle"));
+
+  // set up checkbox
+  const checkbox = document.getElementById("choose-strict");
+
+  let strict = checkbox.checked;
+
+  console.log(strict);
+
+  checkbox.addEventListener("change", toggleStrict);
+  function toggleStrict(){
+    strict = !strict;
+    console.log(strict);
+  }
+
+  //setting up buttons
   const count = document.getElementById("count");
   count.textContent = 0;
-  const circles = Array.from(document.getElementsByClassName("circle"));
 
   const startButton = document.getElementById("start-button");
   startButton.addEventListener("click", startGame);
 
+  const resetButton = document.getElementById("reset-button");
+  resetButton.addEventListener("click", resetGame);
+
+  //starting game
   function startGame(e){
     circles.map(circle => {
       circle.addEventListener("click", addToUserNumbers);
     });
     getRandomIndex();
     e.target.removeEventListener("click", startGame, false);
+    e.target.style.background = "grey";
   }
 
+  //computer chooses a random index
   function getRandomIndex(){
     chosenNumbers.push(arr[Math.floor(Math.random() * arr.length)]);
     count.textContent++;
@@ -25,7 +45,7 @@ document.addEventListener("DOMContentLoaded", function(){
     return chosenNumbers;
   }
 
-
+  //user has to click on button that has the corresponding index
   function addToUserNumbers(e){
     userNumbers.push(parseInt(e.target.getAttribute("data-index"), 10));
     console.log(userNumbers)
@@ -33,34 +53,48 @@ document.addEventListener("DOMContentLoaded", function(){
   }
 
   function isCorrect(arr1, arr2){
+    //user has not yet finished clicking on the buttons
     for(let i = 0; i < arr1.length; i++) {
       if(arr2[i] !== arr1[i]){
-        console.log("sg wrong")
-        console.log(`the correct order: ${arr2}`)
-        userNumbers = [];
-        return false;
+        if(strict){
+          console.log("you have to start again");
+          resetGame();
+          return false;
+        } else {
+          console.log("sg wrong"),
+          console.log(`the correct order: ${arr2}`)
+          userNumbers = [];
+          return false;
+        }
       }
     }
     //if the 2 arrays are of the same length and all their elements are identical
     if (arr1.length === arr2.length){
       if (arr1.length === 5){// win after 5 turns
-        win();
+        handleWin();
       } else {
+        //round finished succcessfully
         console.log("ok");
         getRandomIndex();
+        userNumbers = [];
       }
-      userNumbers = [];
     }
     return true;
   }
 
-  function win(){
+  function handleWin(){
     console.log("You won!!!!");
+    resetGame();
+  }
+
+  function resetGame(){
+    userNumbers = []
     chosenNumbers = [];
     count.textContent = 0;
     circles.map(circle => {
       circle.removeEventListener("click", addToUserNumbers, false);
     });
     startButton.addEventListener("click", startGame);
+    startButton.style.background = "green";
   }
 });
